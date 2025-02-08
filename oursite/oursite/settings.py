e685767ad0bf64a83e5db61d2619cf2c6ec02dd6
omoraisu <dapeladas@up.edu.ph>
2025-02-08 22:24:37 +0800

    Moved static folder

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'blog' / 'static']   # Ensure 'static' folder is included
+STATICFILES_DIRS = [BASE_DIR / 'static']   # Ensure 'static' folder is included
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will store files
 
 # Default primary key field type
