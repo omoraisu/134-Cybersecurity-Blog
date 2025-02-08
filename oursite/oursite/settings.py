990ad872c6af0f42850416752069f8d509aa7a4b
omoraisu <dapeladas@up.edu.ph>
2025-02-08 22:54:38 +0800

    Update static url

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'oursite/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']   # Ensure 'static' folder is included
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will store files
 
