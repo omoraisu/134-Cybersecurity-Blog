224d52dab18958e5032d1192e99c545594697253
omoraisu <dapeladas@up.edu.ph>
2025-02-08 20:46:54 +0800

    Correct static files directory

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# This is required for collectstatic to work
+# This is where Django will collect all static files when you run collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Tell Django where to look for static files in your apps
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "blog", "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
